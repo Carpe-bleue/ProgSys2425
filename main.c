bda597616d49e4b33ce1c4fe11e9c6cc7bf6c16e
carpe-bleue <numero2342@gmail.com>
2024-11-27 15:15:25 +0100

    test, push

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
 //
 // Created by carpe-bleue on 27/11/24.
 //
+#include <unistd.h>
+
+
+#define BUFSIZE 128
+
+int main(int argc, char *argv[]) {
+    int fd = 1;
+    const char *message = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseah \n";
+
+    write(fd,message,70); //fd stands for the permission here 1 is std_out
+
+
+    return(0);
+}
